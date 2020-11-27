fbad51a0340b00ca037b132a85181d27b62d723a
Matheus Ágape <mattnohype@gmail.com>
2020-11-27 07:54:56 +0000

    Código funcionando porem sem a função "BotarGasolina"

--- Main.java
+++ Main.java
@@ -19,36 +19,32 @@ class Main {
   	  if( opcao == 1 ){
       at.sts = true;
       System.out.println(at.status);
-    	BotarGasolina();
+    	System.out.println("***Quantos litros de gasolina deseja colocar?***");
+      litros = entrada.nextInt();
+        switch (litros){
+          case 1:
+            at.atual = 1;
+            break;
+          case 2:
+            at.atual = 2;
+            break;
+          case 3:
+            at.atual = 3;
+            break;
+          case 4:
+            at.atual = 4;
+            break;
+          case 5:
+            at.atual = 5;
+            break;
+          default:
+            entrada.close();
+          }
 	  	} else if(opcao != 1) {
 	    	System.out.println("***Digite 1 para ligar a bomba**\n***Digite 0 para sair do programa**");
 	  	}
-    
-  }  
-  public static void BotarGasolina(){
-    System.out.println("***Quantos litros de gasolina deseja colocar?***");
-    litros = entrada.nextInt();
-    switch (litros){
-    case 1:
-      at.atual = 1;
-      break;
-    case 2:
-      at.atual = 2;
-      break;
-    case 3:
-      at.atual = 3;
-      break;
-    case 4:
-      at.atual = 4;
-      break;
-    case 5:
-      at.atual = 5;
-      break;
-    default:
-      entrada.close();
-  }    
-  
-}
+      System.out.println(at.status);
+  }
 }
 
 
